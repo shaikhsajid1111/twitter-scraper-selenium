813b666cfd0c04b20725045654bc64b3a4f3349e
Sajid Shaikh <shaikhsajid11112000@gmail.com>
2022-10-02 15:14:22 +0530

    upgraded version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setuptools.setup(
     name="twitter_scraper_selenium",
-    version="2.0.0",
+    version="3.0.0",
     author="Sajid Shaikh",
     author_email="shaikhsajid3732@gmail.com",
     description="Python package to scrap twitter's front-end easily with selenium",
