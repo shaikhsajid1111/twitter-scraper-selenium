3faeff4068441c975606f0095144f240677f58bb
Sajid Shaikh <shaikhsajid11112000@gmail.com>
2023-10-02 09:51:06 +0530

    Added deprecated function

---
+++ twitter_scraper_selenium/utils.py
@@ -0,0 +1,8 @@
+import warnings
+import sys
+
+def deprecated_exit_function():
+    """
+    Deprecated: This function is no longer recommended for use.
+    """
+    sys.exit("Deprecation Warning: This function has been deprecated and cannot be used. Exiting...")
